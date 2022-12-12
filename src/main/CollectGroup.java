891c1272d15180593a95a845087de5392481d293
MaryM-0352 <113771940+MaryM-0352@users.noreply.github.com>
2022-12-12 21:04:23 +0500

    Create CollectGroup.java

---
+++ src/main/CollectGroup.java
@@ -0,0 +1,17 @@
+package src.main.java;
+
+import org.telegram.telegrambots.meta.api.objects.Document;
+
+import java.io.IOException;
+import java.util.*;
+
+public class CollectGroup {
+    public static List<List<String>> collectGroup(List<String[]> dictionary) throws IOException {
+        List<List<String>> all_text = new ArrayList<>();
+        for (String[] info: dictionary){
+            DownFileDocx.DownFileDocx(info[0], info[1]);
+            all_text.add(ReadFile.ReadTXTFile(info[0]));
+        }
+        return all_text;
+    }
+}

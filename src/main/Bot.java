8ffca726366d1b11602a04e816f7cf4f832178ae
MaryM-0352 <113771940+MaryM-0352@users.noreply.github.com>
2022-12-12 21:02:27 +0500

    Update Bot.java

--- src/main/Bot.java
+++ src/main/Bot.java
@@ -3,15 +3,19 @@ package src.main.java;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.*;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.*;
 import java.io.File;
-import java.util.List;
+import java.util.*;
 
+
+//ДЕЛА НАСУЩНЫЕ: скачать картинку в норм форме(чтобы читалось)
 public class Bot extends TelegramLongPollingBot {
-    
+    final static public String BOT_TOKEN = Info.token();
+    final private String BOT_NAME = Info.name();
 
     @Override
     public String getBotUsername() {
@@ -23,7 +27,8 @@ public class Bot extends TelegramLongPollingBot {
         return BOT_TOKEN;
     }
 
-
+    List<String[]> group_info = new ArrayList<>();
+    int current_id = -10;
     @Override
     public void onUpdateReceived(Update update) {
         //handle messages -> we look for the message we recieved
@@ -42,47 +47,71 @@ public class Bot extends TelegramLongPollingBot {
                     SendMsg(message, "Hello World!");
                 } else if (msg.equals("/start")) {
                     SendMsg(message, "Ok! let's get the party started 😀\nWhat do you want to convert to PDF?");
-                } else if (msg.startsWith("txt")) {
-                    SendMsg(message, "attach file with txt");
+                } else if (msg.equals("/only_one")){
+                    current_id = message.getMessageId();
+                } else if (msg.equals("/finish")){
+                    try {
+                        GetFilePDF.GetFilePDF("empty", "group", group_info);
+                        String chatId = message.getChatId().toString();
+                        SendDocFile(chatId, "text.txt");
+                        group_info.clear();
+                    } catch (IOException | TelegramApiException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
-            } else if (message.hasDocument()) {
-                try {
-                    var doc = update.getMessage().getDocument();
-                    DownFileDocx.DownFileDocx(doc);
-                    System.out.println("File is downloaded");
-                    String chatId = message.getChatId().toString();
-                    GetFilePDF.GetFilePDF(doc);
-                    System.out.println("File is converted");
-                    SendDocFile(chatId);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                } catch (TelegramApiException e) {
-                    throw new RuntimeException(e);
-                }
-            } else if (message.hasPhoto()) {
-                try {
-                    var doc = update.getMessage().getPhoto();
-                    DownFilePhoto(doc);
-                    //GetFilePDF(doc);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
+            } else {
+                assert message != null;
+                if (message.hasDocument() && message.getDocument() != null) {
+                    try {
+
+                        if (!group_info.isEmpty()){
+                            SendMsg(message, "Please send '/finish' when you're finished sending a group of files." +
+                                                "If you want to add to group one file, send '/only_one'");
+                        }
+                        var groupId = update.getMessage().getMediaGroupId();
+                        final String file_name = message.getDocument().getFileName();
+                        final String file_id = message.getDocument().getFileId();
+                        int id = message.getMessageId();
+                        System.out.println(current_id);
+                        if (id == current_id + 1){
+                            groupId = "group";
+                        }
+                        if (groupId != null){
+                            String[] info = new String[2];
+                            info[0] = file_name; info[1] = file_id;
+                            group_info.add(info);
+                        }
+                        else {
+                            var doc = update.getMessage().getDocument();
+                            DownFileDocx.DownFileDocx(file_name, file_id);
+                            String chatId = message.getChatId().toString();
+                            GetFilePDF.GetFilePDF(file_name, null, group_info);
+                            SendDocFile(chatId, file_name);
+                        }
+                    } catch (IOException | TelegramApiException e) {
+                        throw new RuntimeException(e);
+                    }
+                } else if (message.hasPhoto()) {
+                    try {
+                        String chat_id = update.getMessage().getChatId().toString();
+                        var doc = update.getMessage().getPhoto();
+                        DownFilePhoto.DownFilePhoto(doc);
+                        final String fileId = doc.get(0).getFileId();
+                        GetPhotoPDF.GetPhotoPDF(fileId);
+                        SendDocFile(chat_id, "empty");
+                        //GetFilePDF.GetFilePDF(doc);
+                    } catch (IOException | TelegramApiException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
             }
         }
     }
 
-    private void DownFilePhoto(List<PhotoSize> doc) throws IOException {
-        if (doc != null){
-            final String fileId = doc.get(1).getFileId();
-            final String fileName = doc.get(1).getFileUniqueId();
-            UploadFile.uploadFile(fileName, fileId);
-        }
-    }
-
-
-    public void SendDocFile(String chatId) throws IOException, TelegramApiException {
+    public void SendDocFile(String chatId, String filename) throws IOException, TelegramApiException {
+        System.out.print("It is SendFunction");
         SendDocument sendDocumentRequest = new SendDocument();
-        File name = new File("BlankPdf.pdf");
+        File name = new File(filename + ".pdf");
         sendDocumentRequest.setChatId(chatId);
         sendDocumentRequest.setDocument(new InputFile(name));
         sendDocumentRequest.setCaption("Document");
@@ -92,10 +121,8 @@ public class Bot extends TelegramLongPollingBot {
         private void SendMsg (Message message, String s){
             SendMessage sendMessage = new SendMessage();
 
-            //set the destination, and the text we want to send
             sendMessage.setChatId(message.getChatId().toString());
             sendMessage.setText(s);
-            //try to send it:
             try {
                 execute(sendMessage);
             } catch (TelegramApiException e) {

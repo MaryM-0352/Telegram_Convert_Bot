be77a98cf9a903bdabdaf5b128dc992d5292c538
MaryM-0352 <113771940+MaryM-0352@users.noreply.github.com>
2022-12-12 21:02:53 +0500

    Update GetFilePDF.java

--- src/main/GetFilePDF.java
+++ src/main/GetFilePDF.java
@@ -8,35 +8,70 @@ import org.telegram.telegrambots.meta.api.objects.Document;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class GetFilePDF {
-    public static Long GetFilePDF(Document document) throws IOException {
-        System.out.println("You are in PDFConverter");
-        String text = "";
-        if (ReadFile.ReadTXTFile(document) == null){
-             text = ReadFile.ReadDOCXFile(document);
+    public static Long GetFilePDF(String file_name, String key, List<String[]> dictionary) throws IOException {
+        List<String> text = new ArrayList<>();
+        List<List<String>> text1 = new ArrayList<>();
+        if (key == null){
+            if (ReadFile.ReadTXTFile(file_name) == null){
+                System.out.println(' ');
+                //text = ReadFile.ReadDOCXFile(document);
+            }
+            else{
+                text = ReadFile.ReadTXTFile(file_name);
+            }
         }
-        else{
-             text = ReadFile.ReadTXTFile(document);
+        else {
+            text1 = CollectGroup.collectGroup(dictionary);
         }
+        String name = "";
 
-        System.out.println(text);
         PDDocument pdf_doc = new PDDocument();
-        pdf_doc.addPage(new PDPage());
-        PDPage page = pdf_doc.getPage(0);
-        PDPageContentStream stream = new PDPageContentStream(pdf_doc, page);
-        stream.beginText();
-        stream.newLineAtOffset(25, 700);
-        stream.setFont(PDType1Font.TIMES_ROMAN, 12);
-        stream.showText(text);
-        stream.endText();
-        stream.close();
+        if (Objects.equals(key, "group")){
+            int count = 0;
+            for (List<String> file: text1){
+                System.out.println("Here we go again");
+                pdf_doc.addPage(new PDPage());
+                PDPage page = pdf_doc.getPage(count);
+                PDPageContentStream stream = new PDPageContentStream(pdf_doc, page);
+                stream.beginText();
+                stream.setFont(PDType1Font.TIMES_ROMAN, 12);
+                stream.setLeading(14.5f);
+                stream.newLineAtOffset(25, 700);
+                for (String s : file) {
+                    stream.showText(s);
+                    stream.newLine();
+                }
+                stream.endText();
+                stream.close();
+                count += 1;
+            }
+            name = "text.txt.pdf";
+        } else if (text != null){
+            pdf_doc.addPage(new PDPage());
+            PDPage page = pdf_doc.getPage(0);
+            PDPageContentStream stream = new PDPageContentStream(pdf_doc, page);
+            stream.beginText();
+            stream.setFont(PDType1Font.TIMES_ROMAN, 12);
+            stream.setLeading(14.5f);
+            stream.newLineAtOffset(25, 700);
+            for (String s : text) {
+                stream.showText(s);
+                stream.newLine();
+            }
+            stream.endText();
+            stream.close();
+            name = file_name + ".pdf";
+        }
 
-
-
-        String name = "BlankPdf.pdf";
         pdf_doc.save(new File(name));
         pdf_doc.close();
+        System.out.println("pdf converter is final");
         return pdf_doc.getDocumentId();
     }
 }

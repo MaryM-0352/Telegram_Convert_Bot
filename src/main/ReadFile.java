cf42e608a08bb73e513febc5c46b06fb79a7cd56
MaryM-0352 <113771940+MaryM-0352@users.noreply.github.com>
2022-12-12 21:03:28 +0500

    Update ReadFile.java

--- src/main/ReadFile.java
+++ src/main/ReadFile.java
@@ -11,41 +11,55 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ReadFile {
-    public static String ReadTXTFile(Document document) throws IOException {
-        Path content_path = Paths.get("C:\\Users\\Maria\\IdeaProjects\\Telegram_Convert_Bot1\\src\\main\\resources\\upl_files\\", document.getFileName());
-        System.out.println(content_path);
+    public static List<String> ReadTXTFile(String filename) throws IOException {
+        Path content_path = Paths.get("C:\\Users\\Maria\\IdeaProjects\\Telegram_Convert_Bot1" +
+                                           "\\src\\main\\resources\\upl_files\\", filename);
         BufferedReader reader = Files.newBufferedReader(content_path);
         String result = "";
-        int c;
+        int c, k = 0;
+        List<String> result1 = new ArrayList<>();
         try {
             while ((c = reader.read()) != -1) {
-                result += (char) c;
-            }
-            ;
+                if (c == 9){
+                    result += "    ";
+                }
+                else if (c != 13){
+                    result += (char) c;
+                }
+                else{
+                    result1.add(k, result);
+                    k += 1;
+                    result = "";
+                    c = reader.read();
+                }
+            };
+            result1.add(k, result);
         } catch (IOException e){
             return null;
         }
-        return result;
+        return result1;
     }
 
     public static String ReadDOCXFile(Document document) {
-        String text = "";
+        StringBuilder text = new StringBuilder();
         try {
-
             File file = new File("C:\\Users\\Maria\\IdeaProjects\\Telegram_Convert_Bot1\\src\\main\\resources\\upl_files\\" + document.getFileName());
             FileInputStream fis = new FileInputStream(file.getAbsolutePath());
             XWPFDocument document1 = new XWPFDocument(fis);
             List<XWPFParagraph> paragraphs = document1.getParagraphs();
             for (XWPFParagraph para : paragraphs) {
-                text += para.getText();
+                System.out.println(para.getText());
+                text.append(para.getText());
             }
             fis.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return text;
+
+        return text.toString();
     }
 }
